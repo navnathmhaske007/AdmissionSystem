c38fdb8872a013e3930ec78fc09ea2a6ec4448f3
navnathmhaske007 <navnathmhaske65@gmail.com>
2022-02-15 11:07:40 +0530

    Add files via upload

---
+++ src/test/java/com/rscoe/admissionsystem/AdmissionsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rscoe.admissionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmissionsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
