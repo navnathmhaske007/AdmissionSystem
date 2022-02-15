aa69658475b05307bb8a85a47df0bd2f3d1a03be
navnathmhaske007 <navnathmhaske65@gmail.com>
2022-02-15 10:59:58 +0530

    Add files via upload

---
+++ src/main/java/com/rscoe/admissionsystem/AdmissionsystemApplication.java
@@ -0,0 +1,13 @@
+package com.rscoe.admissionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmissionsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmissionsystemApplication.class, args);
+	}
+
+}
